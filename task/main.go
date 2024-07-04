5817560b85849b868660c87ba66848cd55b0b451
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-04 15:08:03 +0400

    [master] 4/7 Choose your path Description:
    Good news everyone: our university keeps growing larger! Five departments have been established; now our potential students can apply to the Mathematics, Physics, Biotech, Chemistry, or the Engineering Department. Each applicant needs to choose three departments and rank them. First, the department with the highest priority; second, the department in case the first option doesn't work out. The third department is Plan C.
    
    Your task for this stage is to develop an algorithm that will sort the applicants according to their GPA and take into account their priorities: if the applicant doesn't score high enough to get accepted to the department of first priority, they'll participate in the rankings for the second priority, and so on.
    
    In other words, the admission algorithm should work by the following rules:
    
    1. Firstly, the ranking list for every department is created according to the applicant's first priorities. People who chose Physics as their first priority only participate in ranking for the Physics department, and so on.
    2. Applicants are scored according to their GPA. People who have a higher GPA score are ranked higher in the department's ranking list.
    3. Each department accepts N (maximum number of students for the department) best students from the department's ranking list. If there are fewer than N students on the department's list, all students from the list are accepted.
    4. The accepted students are removed from the general list of applicants and no longer participate in the ranking.
    5. The same procedure is repeated for the second priorities. If there are departments that accepted fewer than N students in the first stage of admission, these departments try to accept more students to fill all N student positions.
    6. The same procedure is repeated for the third priority.
    
    Objectives:
    
    1. Read an N integer from the input. This integer represents the maximum number of students for each department.
    2. Read the file named applicants.txt (this file is already included in the project's files, even though it is not visible; so you only need to download it if you want to take a closer look at it). Each line equals one applicant, their first name, last name, GPA, first priority department, second priority department, and third priority department. Columns with values are separated by whitespace characters. For example, Laura Spungen 3.71 Physics Engineering Mathematics.
    3. Sort applicants according to their GPA and priorities (and names, if their GPA scores are the same). As in the previous stage, if two applicants to the same department have the same GPA, sort them by their full names in alphabetical order.
    4. For each department, choose the N best candidates. Some departments are less popular than others, so there may be fewer available candidates for a department. However, their number shouldn't be more than N.
    5. Print the departments in the alphabetic order (Biotech, Chemistry, Engineering, Mathematics, Physics), output the names and the GPA of enrolled applicants for each department. Separate the name and the GPA with a whitespace character. Here's an example (you may add empty lines between the departments' lists to increase the text readability):
    
    department_name
    applicant1 GPA1
    applicant2 GPA2
    applicant3 GPA3
    <...>

--- task/main.go
+++ task/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
+
+type Department struct {
+	name       string
+	applicants []*Applicant
+	waves      [][]*Applicant
+}
 
 type Applicant struct {
 	firstName, lastName string
 	GPA                 float64
+	department          *Department
 }
 
 func (applicant *Applicant) fullName() string {
@@ -15,43 +27,128 @@ func (applicant *Applicant) fullName() string {
 }
 
 func main() {
-	// Read the first input, an N integer representing the total number of applicants
-	var numberOfApplicants int
-	fmt.Scan(&numberOfApplicants)
-
-	// Read the second input, an M integer representing the number of applicants that should be accepted to the university.
-	var shouldBeAccepted int
-	fmt.Scan(&shouldBeAccepted)
-
-	if numberOfApplicants < shouldBeAccepted || shouldBeAccepted == 0 {
-		return
+	// Read an N integer from the input. This integer represents the maximum number of students for each department.
+	var maxStudentsInDepartment int
+	fmt.Scan(&maxStudentsInDepartment)
+	// Read the file named applicants.txt (this file is already included in the project's files, even though it is not visible; so you only need to download it if you want to take a closer look at it).
+	file, err := os.Open("applicants.txt")
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// Read N lines from the input.
-	// Each line contains the first name, the last name, and the applicant's GPA.
-	// These values are separated by one whitespace character. A GPA is a floating-point number with two decimals.
-
-	// Write your code solution for the project below.
-	applicants := make([]Applicant, numberOfApplicants)
-	for i := range applicants {
-		fmt.Scanf("%s %s %f", &applicants[i].firstName, &applicants[i].lastName, &applicants[i].GPA)
+	departments := map[string]Department{
+		"Mathematics": {"Mathematics", make([]*Applicant, maxStudentsInDepartment), make([][]*Applicant, 3)},
+		"Physics":     {"Physics", make([]*Applicant, maxStudentsInDepartment), make([][]*Applicant, 3)},
+		"Biotech":     {"Biotech", make([]*Applicant, maxStudentsInDepartment), make([][]*Applicant, 3)},
+		"Chemistry":   {"Chemistry", make([]*Applicant, maxStudentsInDepartment), make([][]*Applicant, 3)},
+		"Engineering": {"Engineering", make([]*Applicant, maxStudentsInDepartment), make([][]*Applicant, 3)},
+	}
+	// Each line equals one applicant, their first name, last name, GPA, first priority department, second priority department, and third priority department.
+	// Columns with values are separated by whitespace characters. For example, Laura Spungen 3.71 Physics Engineering Mathematics.
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		info := strings.Split(scanner.Text(), " ")
+		GPA, err1 := strconv.ParseFloat(info[2], 64)
+		if err1 != nil {
+			fmt.Println("Error parsing GPA", info)
+			continue
+		}
+		applicant := Applicant{
+			info[0],
+			info[1],
+			GPA,
+			nil,
+		}
+		d1, d2, d3 := departments[info[3]], departments[info[4]], departments[info[5]]
+		d1.waves[0] = append(d1.waves[0], &applicant)
+		d2.waves[1] = append(d2.waves[1], &applicant)
+		d3.waves[2] = append(d3.waves[2], &applicant)
 	}
 
-	// Output the Successful applicants: message.
-	fmt.Println("Successful applicants:")
+	// Sort applicants according to their GPA and priorities (and names, if their GPA scores are the same).
+	// As in the previous stage, if two applicants to the same department have the same GPA, sort them by their full names in alphabetical order.
 
-	sort.Slice(applicants, func(i, j int) bool {
-		if applicants[i].GPA == applicants[j].GPA {
-			return applicants[i].fullName() < applicants[j].fullName()
+	sortApplicants := func(applicants []*Applicant) {
+		sort.Slice(applicants, func(i, j int) bool {
+			if applicants[i] == nil {
+				return false
+			} else if applicants[j] == nil {
+				return true
+			}
+
+			if applicants[i].GPA == applicants[j].GPA {
+				return applicants[i].fullName() < applicants[j].fullName()
+			}
+
+			return applicants[i].GPA > applicants[j].GPA
+		})
+	}
+
+	// first of all, let's try to sort out first wave, then second, then third
+	distribute := func(department *Department, waveNumber int) {
+		// sort applicants by GPA + fullName
+		sortApplicants(department.waves[waveNumber])
+
+		index, capacity := 0, len(department.applicants)
+		for _, applicant := range department.applicants {
+			if applicant == nil {
+				break
+			}
+			index++
 		}
 
-		return applicants[i].GPA > applicants[j].GPA
-	})
-	// Output M lines for applicants with the top-ranking GPAs.
-	// Each line should contain the first and the last name of the applicant separated by a whitespace character.
-	// Successful applicants' names should be printed in descending order depending on the GPA — first, the applicant with the best GPA, then the second-best, and so on.
-	// If two applicants have the same GPA, rank them in alphabetical order using their full names (we know it's not really fair to choose students by their names, but what choice do we have?)
-	for _, applicant := range applicants[:shouldBeAccepted] {
-		fmt.Println(applicant.fullName())
+		for _, applicant := range department.waves[waveNumber] {
+			if index >= capacity {
+				return
+			}
+
+			if applicant.department == nil {
+				department.applicants[index] = applicant
+				applicant.department = department
+				index++
+			}
+		}
+	}
+
+	// distribute first wave
+	for _, department := range departments {
+		distribute(&department, 0)
+	}
+	// distribute second wave
+	for _, department := range departments {
+		distribute(&department, 1)
+	}
+	// distribute third wave
+	for _, department := range departments {
+		distribute(&department, 2)
+	}
+
+	// Print the departments in the alphabetic order (Biotech, Chemistry, Engineering, Mathematics, Physics), output the names and the GPA of enrolled applicants for each department.
+	var departmentNames []string
+	for departmentName := range departments {
+		departmentNames = append(departmentNames, departmentName)
+	}
+	sort.Strings(departmentNames)
+	// Some departments are less popular than others, so there may be fewer available candidates for a department.
+	// However, their number shouldn't be more than N.
+	// Separate the name and the GPA with a whitespace character. Here's an example (you may add empty lines between the departments' lists to increase the text readability):
+	for _, departmentName := range departmentNames {
+		department := departments[departmentName]
+		// department_name
+		fmt.Println(department.name)
+		// sort applicants by GPA & names
+		sortApplicants(department.applicants)
+		for _, applicant := range department.applicants {
+			if applicant == nil {
+				break
+			}
+
+			// applicant1 GPA1
+			// applicant2 GPA2
+			// applicant3 GPA3
+			// <...>
+			fmt.Printf("%s %.2f\n", applicant.fullName(), applicant.GPA)
+		}
+		fmt.Println()
 	}
 }
